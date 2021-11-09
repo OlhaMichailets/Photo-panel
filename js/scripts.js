52c428ede16df3a21527fc503d37bca1cebf5342
Olha <32494251+OlhaMichailets@users.noreply.github.com>
2021-11-09 21:09:48 +0200

    fixed script for footer button zadozwon

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,69 @@
 let openCalcPopUpButton = document.querySelector('.goCalcButton');
-openCalcPopUpButton.addEventListener('click', function () {
-    document.querySelector('.popupCalculator').classList.add('popupShow');
-});
+if (openCalcPopUpButton) {
+    openCalcPopUpButton.addEventListener('click', function () {
+        document.querySelector('.popupCalculator').classList.add('popupShow');
+    });
+};
 
 
 let closeCalcPopUpButton = document.querySelector('.popCalcCloseIcon');
-closeCalcPopUpButton.addEventListener('click', function () {
-    document.querySelector('.popupCalculator').classList.remove('popupShow');
-});
+if (closeCalcPopUpButton) {
+    closeCalcPopUpButton.addEventListener('click', function () {
+        document.querySelector('.popupCalculator').classList.remove('popupShow');
+    });
+};
 
 
 let openQuestPopUpButton = document.querySelector('.calculateButton');
-openQuestPopUpButton.addEventListener('click', function (){
-    document.querySelector('.popupCalculator').classList.remove('popupShow');
-    document.querySelector('.popupQuestion').classList.add('popupShow');
-})
+if (openQuestPopUpButton) {
+    openQuestPopUpButton.addEventListener('click', function (){
+        document.querySelector('.popupCalculator').classList.remove('popupShow');
+        document.querySelector('.popupQuestion').classList.add('popupShow');
+});
+};
 
 
 let closeQuestPopUpButton = document.querySelector('.popQuestCloseIcon');
-closeQuestPopUpButton.addEventListener('click', function () {
-    document.querySelector('.popupQuestion').classList.remove('popupShow');
-})
+if (closeQuestPopUpButton) {
+    closeQuestPopUpButton.addEventListener('click', function () {
+        document.querySelector('.popupQuestion').classList.remove('popupShow');
+    });
+};
 
 
 let returnCalcPopUpButton = document.querySelector('.returnCalculatorButton');
-returnCalcPopUpButton.addEventListener('click', function () {
-    document.querySelector('.popupQuestion').classList.remove('popupShow');
-    document.querySelector('.popupCalculator').classList.add('popupShow');
-})
+if (returnCalcPopUpButton) {
+    returnCalcPopUpButton.addEventListener('click', function () {
+        document.querySelector('.popupQuestion').classList.remove('popupShow');
+        document.querySelector('.popupCalculator').classList.add('popupShow');
+    });
+};
 
 
 let questionForm = document.querySelector('.questionForm');
-questionForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    document.querySelector('.popupQuestion').classList.remove('popupShow');
-    document.querySelector('.feedback').classList.add('popupShow');
-})
+if (questionForm) {
+    questionForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        document.querySelector('.popupQuestion').classList.remove('popupShow');
+        document.querySelector('.feedback').classList.add('popupShow');
+    });
+};
 
 
 let closeFeedbackFormIcon = document.querySelector('.popFeedbackCloseIcon');
-closeFeedbackFormIcon.addEventListener('click', function () {
-    document.querySelector('.feedback').classList.remove('popupShow');
-})
+if (closeFeedbackFormIcon) {
+    closeFeedbackFormIcon.addEventListener('click', function () {
+        document.querySelector('.feedback').classList.remove('popupShow');
+    });
+};
 
 
 let closeFeedbackForm = document.querySelector('.feedbackButton');
-closeFeedbackForm.addEventListener('click', function () {
-    document.querySelector('.feedback').classList.remove('popupShow');
-})
+if (closeFeedbackForm) {
+    closeFeedbackForm.addEventListener('click', function () {
+        document.querySelector('.feedback').classList.remove('popupShow');
+    });
+};
 
 
 let openCallbackForm = document.querySelector('.callbackButton');
